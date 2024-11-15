74d28bc2e903d3fc72b2f55b7a9ae6b06ab31805
Aidan Diaz <diaz.aidan.d@gmail.com>
2024-11-15 11:25:54 -0500

    fixed thumbs up button

--- app/routes.js
+++ app/routes.js
@@ -18,6 +18,7 @@ module.exports = function(app, passport, db) {
         })
     });
 
+
     // LOGOUT ==============================
     app.get('/logout', function(req, res) {
         req.logout(() => {
